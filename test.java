05718ea356eb91d392642fdfed5ba6c3218b294c
aleBenavides <benavijuale@gmail.com>
2024-02-26 14:25:59 -0600

    se modifica Test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(" do");
+        System.out.println("re");
+        System.out.println("mi");
+        
+
+    }
+}
