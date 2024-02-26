cf4e744c77581b7265f5a36fa54b2ee3baeccdb5
aleBenavides <benavijuale@gmail.com>
2024-02-26 14:12:06 -0600

    se modifica Test.java y archivo.txt

---
+++ test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(" Mundo");
+        System.out.println("Hola do");
+        System.out.println("Hola Mudo");
+        System.out.println("Hla Mundo");
+        System.out.println("Hola Mndo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hol Mundo");
+
+    }
+}
